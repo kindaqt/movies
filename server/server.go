4581786fe95e6d10d803b1744fc377da441ac7c8
William Heiderman <heiderman.william@gmail.com>
2020-05-19 19:48:14 -0400

    healthz handler

--- server/server.go
+++ server/server.go
@@ -4,10 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func Router() {
 	r := gin.Default()
-	r.GET("/healthz", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "healthy",
-		})
-	})
+	r.GET("/healthz", healthzHandler)
 	r.Run()
+}
+
+func healthzHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "healthy",
+	})
 }
