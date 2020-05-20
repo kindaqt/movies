f2ad1da4c91a09b86ee30e6284da10cd827cb153
William Heiderman <heiderman.william@gmail.com>
2020-05-20 10:09:17 -0400

    Handlers package. Middleware package.

--- server/server.go
+++ server/server.go
@@ -1,33 +1,15 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"hello/server/handlers"
+	"hello/server/middleware"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Router() *gin.Engine {
 	r := gin.Default()
-	r.Use(CORSMiddleware())
-	r.GET("/healthz", healthzHandler)
+	r.Use(middleware.CORSMiddleware())
+	r.GET("/healthz", handlers.HealthzHandler)
 	return r
-}
-
-func healthzHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"status": "healthy",
-	})
-	// c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
-}
-
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
 }
